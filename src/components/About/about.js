bf2c22110c90af2c4216461b6eafcb5b081ba8c5
Ritesh Birthal <riteshbirthal@gmail.com>
2025-05-12 05:16:51 +0530

    Added Responsiveness

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -18,21 +18,6 @@ function About(){
                     </div>
 
                     <div className="card">
-                        <h2>Technical Skills</h2>
-                        <p>I have extensive experience with a range of technologies across both front-end and back-end development. My primary technical skills include: C++, Python, JavaScript, React.js, FastAPI, Streamlit, MongoDB, etc.
-                        </p>
-                        <button onClick={() => scrollToSection('skills')}><i className='bx bx-send' ></i> For More Info Click Here...</button>
-                    </div>
-
-
-                    <div className="card">
-                        <h2>Personal Attributes</h2>
-                        <p>I'm a problem-solver by nature. I believe in the power of continuous learning and always seek to stay updated with the latest advancements in software development. I am detail-oriented and take pride in writing clean, well-documented code that is easy to maintain and scale.</p>
-                        <p>In addition to my technical skills, I value collaboration and enjoy working with diverse teams to deliver the best possible solutions. I am open to feedback and constantly strive to improve my craft, whether through personal projects or professional experience.</p>
-                    </div>
-                </div>
-                <div className="col2">
-                    <div className="card">
                         <h2>Introduction</h2>
                         <p>I'm Ritesh Birthal, a passionate Full-Stack Developer with a deep interest in both front-end and back-end development. With a background in Physics from IIT Kanpur and experience at Carelon Global Solutions, I focus on creating high-performance, scalable, and user-friendly web applications. I thrive on solving complex problems and delivering high-quality solutions that align with user needs.</p>
                     </div>
@@ -40,6 +25,21 @@ function About(){
                     <div className="card">
                         <h2>Education</h2>
                         <p>I hold a BS-MS dual degree in Physics from the Indian Institute of Technology Kanpur, where I gained a strong foundation in computational sciences, data structures, algorithms, and software engineering principles. My coursework in machine learning, image processing, and advanced numerical methods has been invaluable in tackling real-world problems.</p>
+                    </div>
+
+                </div>
+                <div className="col2">
+                    <div className="card">
+                        <h2>Technical Skills</h2>
+                        <p>I have extensive experience with a range of technologies across both front-end and back-end development. My primary technical skills include: C++, Python, JavaScript, React.js, FastAPI, Streamlit, MongoDB, etc.
+                        </p>
+                        <button onClick={() => scrollToSection('skills')}><i className='bx bx-send' ></i> For More Info Click Here...</button>
+                    </div>
+                    
+                    <div className="card">
+                        <h2>Personal Attributes</h2>
+                        <p>I'm a problem-solver by nature. I believe in the power of continuous learning and always seek to stay updated with the latest advancements in software development. I am detail-oriented and take pride in writing clean, well-documented code that is easy to maintain and scale.</p>
+                        <p>In addition to my technical skills, I value collaboration and enjoy working with diverse teams to deliver the best possible solutions. I am open to feedback and constantly strive to improve my craft, whether through personal projects or professional experience.</p>
                     </div>
 
                     <div className="card">
